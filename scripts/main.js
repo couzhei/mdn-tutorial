99edc5619b90f4dcd8209cb6cb46e659e55e1335
couzhei <couzhei@gmail.com>
2023-06-13 18:05:26 +0330

    I added a button to change the theme of the website by clicking it

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,24 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+setTimeout(main, 1000);
+
+function main(){
+    const myHeading = document.querySelector("h1");
+    myHeading.textContent = "Hello world!";
+    // document.querySelector("html").addEventListener("click", function () {
+    //     alert("Ouch! Stop poking me!");
+    //   });
+    var colorButton = document.getElementById("colorButton");
+    var themeStylesheet = document.getElementById("themeStylesheet");
+
+    var currentTheme = "theme1";
+
+    colorButton.addEventListener("click", function() {
+    if (currentTheme === "theme1") {
+        currentTheme = "theme2";
+        myHeading.textContent = "Theme 2 loaded!";
+    } else {
+        currentTheme = "theme1";
+        myHeading.textContent = "Theme 1 loaded!";
+    }
+    themeStylesheet.href = "styles/" + currentTheme + "-style.css";
+    });
+}
